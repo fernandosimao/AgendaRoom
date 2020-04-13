4525ec8e3959139e606bba7f29f7b73d8770255d
fernandosimao <fernandosimaorj@gmail.com>
2020-04-12 22:19:47 -0300

    Commit até aula 2 do Curso Alura Room Parte 1

--- app/src/main/java/br/com/alura/agenda/model/Aluno.java
+++ app/src/main/java/br/com/alura/agenda/model/Aluno.java
@@ -6,6 +6,8 @@ import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 @Entity
 public class Aluno implements Serializable {
@@ -15,11 +17,13 @@ public class Aluno implements Serializable {
     private int id = 0;
 
     private String nome;
-//    private String sobrenome;
+    //    private String sobrenome;
     private String telefone;
     private String email;
+    //@ignore caso não queira persistir o atributo no SQLITE
+    private Calendar momentoDeCadastro = Calendar.getInstance();
 
-    @Ignore //quando há varios contrutores o Room só considerará um deles (o sem @Ignore)
+    @Ignore //quando há varios contrutores o Room só considerará um deles (o sem @Ignore). Os dois construtores abaixo podem ser apagados
     public Aluno(String nome, String telefone, String email) {
         this.nome = nome;
         this.telefone = telefone;
@@ -38,6 +42,17 @@ public class Aluno implements Serializable {
 //    public void setSobrenome(String sobrenome) {
 //        this.sobrenome = sobrenome;
 //    }
+
+    //foram necessários ser criados os geters e seters para momentoCadastro para que o Room possa colocar esses dados
+    // no banco após a migration. No caso específico foi preciso criar a classe ConversorCalendar, já que o tipo Calendar
+    // não é supoirtado no SQLITE
+    public Calendar getMomentoDeCadastro() {
+        return momentoDeCadastro;
+    }
+
+    public void setMomentoDeCadastro(Calendar momentoDeCadastro) {
+        this.momentoDeCadastro = momentoDeCadastro;
+    }
 
     public void setNome(String nome) {
         this.nome = nome;
@@ -83,5 +98,11 @@ public class Aluno implements Serializable {
 
     public String getNomeCompleto() {
         return nome + " ";
+    }
+
+    public String dataFormatada(){
+        SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+        return formatador.format(momentoDeCadastro.getTime());
+
     }
 }
